4b752ade2469267b6232825878b453b3f9f6250b
Gerson-Solano <83689574+Gerson-Solano@users.noreply.github.com>
2023-12-22 12:35:26 -0600

    Add principals files
    create proyect .net 6 with n-layer

---
+++ SystemIA/SystemIA.WebAplication/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SystemIA.WebAplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
